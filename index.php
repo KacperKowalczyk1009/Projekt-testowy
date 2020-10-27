054bb5a2b579779bc9416b651b0059e9a316e132
KacperKowalczyk1009 <73532503+KacperKowalczyk1009@users.noreply.github.com>
2020-10-27 12:59:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "172.16.131.125";
+$username = "z_ghj";
+$password = "cxz";
+$dbname = "z_ghj";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+echo("<h1>Kacper Kowalczyk NR 4</h1>");
+
+
+}
+echo('</table>');
+
+?>
